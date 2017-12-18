d21d4e1db290a18dc741c348e89db443b7bc9577
xudafeng <xudafeng@126.com>
2017-12-18 17:37:47 +0800

    tweak testcase

--- test/debugger-board.test.js
+++ test/debugger-board.test.js
@@ -6,30 +6,36 @@ import {
 } from './helper';
 
 describe('test/debugger-board.test.js', () => {
-  before(() => {
-    return driver
-      .initWindow({
-        width: 375,
-        height: 667,
-        deviceScaleFactor: 2
-      });
+
+  describe('page func testing', () => {
+
+    before(() => {
+      return driver
+        .initWindow({
+          width: 375,
+          height: 667,
+          deviceScaleFactor: 2
+        });
+    });
+
+    afterEach(function () {
+      return driver
+        .coverage()
+        .saveScreenshots(this);
+    });
+
+    after(() => {
+      return driver
+        .openReporter(false)
+        .quit();
+    });
+
+    it('page render should be ok', () => {
+      return driver
+        .get(BASE_URL)
+        .sleep(1000);
+    });
+
   });
 
-  afterEach(function () {
-    return driver
-      .coverage()
-      .saveScreenshots(this);
-  });
-
-  after(() => {
-    return driver
-      .openReporter(false)
-      .quit();
-  });
-
-  it('page render should be ok', () => {
-    return driver
-      .get(BASE_URL)
-      .sleep(1000);
-  });
 });
