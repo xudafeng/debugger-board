6d3a3098800814432c1ddf52060990cd551e9a9f
xudafeng <xudafeng@126.com>
2018-03-11 15:15:13 +0800

    cov testcase

--- test/debugger-board.test.js
+++ test/debugger-board.test.js
@@ -3,6 +3,7 @@
 import {
   webpackHelper,
 } from 'macaca-wd';
+import Promise from 'bluebird';
 
 const {
   driver,
@@ -19,7 +20,8 @@ describe('test/debugger-board.test.js', () => {
           width: 375,
           height: 667,
           deviceScaleFactor: 2
-        });
+        })
+        .getUrl(BASE_URL);
     });
 
     afterEach(function () {
@@ -34,10 +36,41 @@ describe('test/debugger-board.test.js', () => {
         .quit();
     });
 
-    it('page render should be ok', () => {
+    it('panel should be show', () => {
       return driver
-        .getUrl(BASE_URL)
-        .sleep(1000);
+        .sleep(3000)
+        .elementTouch('#_debugger_board_app > div._debugger_board_thumbnail', {
+          type: 'start'
+        });
+    });
+
+    it('panel should be switch', () => {
+      const queue = [1, 2, 3, 4, 3, 2, 1];
+
+      return driver
+        .sleep(1000)
+        .then(() => {
+          return Promise.reduce(queue, (i, item) => {
+            const selector = `div._debugger_board_nav._debugger_board_common_bar > span:nth-child(${item})`;
+            return driver
+              .elementTouch(selector, {
+                type: 'start'
+              })
+              .sleep(1000);
+          });
+        });
+    });
+
+    it('addLog should be execute', () => {
+      const queue = [1, 2, 3, 4, 3, 2, 1];
+
+      return driver
+        .sleep(1000)
+        .execute(`window.addLog('${Date.now()}')`)
+        .execute(`window.addLog('${Date.now()}')`)
+        .execute(`window.addLog('${Date.now()}')`)
+        .execute(`window.addLog('${Date.now()}')`)
+        .execute(`window.addLog('${Date.now()}')`);
     });
 
   });
