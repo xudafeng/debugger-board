40a6fd6e07b983de752499a53eebc17424a1097c
xudafeng <xudafeng@126.com>
2020-11-24 13:55:40 +0800

    feat: tweak board ui

--- src/index.js
+++ src/index.js
@@ -15,13 +15,11 @@ window._debugger_board = window._debugger_board || {
     const button = document.createElement('div');
     button.id = identifer;
     element.appendChild(button);
-    window[`${identifer}_button_ref`] = React.createRef();
-    window[`${identifer}_drawer_ref`] = React.createRef();
-    ReactDOM.render(<App ref={ele => { window[`${identifer}_button_ref`] = ele}} />, button);
+    ReactDOM.render(<App/>, button);
     const drawer = document.createElement('div');
     drawer.id = `${identifer}_drawer`;
     element.appendChild(drawer);
-    ReactDOM.render(<Drawer ref={ele => { window[`${identifer}_drawer_ref`] = ele}} />, drawer);
+    ReactDOM.render(<Drawer/>, drawer);
   },
   /* eslint-disable */
   version: VERSION
